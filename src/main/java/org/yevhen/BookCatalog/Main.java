f39df96960b2488ed056ee70a19c4459e96af9c0
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2025-02-04 17:31:04 +0200

    second commit

--- src/main/java/org/yevhen/BookCatalog/Main.java
+++ src/main/java/org/yevhen/BookCatalog/Main.java
@@ -1,4 +1,14 @@
 package org.yevhen.BookCatalog;
 
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        BookCollection bc = new BookCollection();
+
+        // get price of book called Tom Jones in EUR
+        System.out.println(bc.findBookByName("Tom Jones").getPrice().convert("EUR"));
+    }
 }
